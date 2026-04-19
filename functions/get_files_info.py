cb8bb55dcd13c9e6d8a67d466e460b177465d211
perryhowe <perry.howe@gmail.com>
2026-04-19 11:05:34 +0100

    feat: add ability to list contents of dir and metadata. add calc app for test

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,23 @@
+import os
+
 def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+    valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+    if valid_target_dir == False:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    try:
+        if os.path.isdir(target_dir) == False:
+            return f'Error: "{directory}" is not a directory'
+        
+        item_string = ""
+
+        for item in os.listdir(target_dir):
+            file_size = os.path.getsize(os.path.join(target_dir, item))
+            is_dir = os.path.isdir(os.path.join(target_dir, item))
+            item_string += f"- {item}: file_size={file_size} bytes, is_dir={is_dir}\n"
+        return item_string
+    
+    except Exception as e:
+        return f"Error: {e}"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,19 +13,25 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=user_prompt
+    model='gemini-2.5-flash', contents=messages
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("Failed API request")
-    
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response: {response.text}")
+    else:
+        print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
