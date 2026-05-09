bd110b57328b807008ac7aea00c5ca9d726d3227
perryhowe <perry.howe@gmail.com>
2026-05-09 16:09:44 +0100

    feat: add system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -20,7 +21,9 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages
+    model='gemini-2.5-flash',
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
     )
 
     if response.usage_metadata == None:
